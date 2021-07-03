54927e774dc0ef83e2754d79f4f6f07a2c67025d
diego <diegobarrosflf@gmail.com>
2021-07-02 21:44:28 -0300

    Solução? Liskov Substitution Principle: - Verificar se o uso da herança gera impactos: Um terceiricado não dever ser promovido nem receber aumento, porém com o uso da herana os métodos que aceitam um Funcionario também aceitavam um Terceirizado; - Usando a comppsição um Terceirizado possue tudo que precisa que são apenas os dados pessoais. - Criamos uma classe para armazenar os dados pessoais que ajuda a compor Funcionario e Terceirizado.

--- src/br/com/alura/rh/model/Terceirizado.java
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -2,12 +2,13 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario{
+public class Terceirizado {
 
+    private final DadosPessoais dadosPessoais;
     private String emppresa;
 
-    public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-        super(nome, cpf, cargo, salario);
+    public Terceirizado(DadosPessoais dadosPessoais ) {
+        this.dadosPessoais = dadosPessoais;
     }
 
     public String getEmppresa() {
@@ -17,4 +18,37 @@ public class Terceirizado extends Funcionario{
     public void setEmppresa(String emppresa) {
         this.emppresa = emppresa;
     }
+
+    public String getNome() {
+        return dadosPessoais.getNome();
+    }
+
+    public void setNome(String nome) {
+        dadosPessoais.setNome(nome);
+    }
+
+    public String getCpf() {
+        return dadosPessoais.getCpf();
+    }
+
+    public void setCpf(String cpf) {
+        dadosPessoais.setCpf(cpf);
+    }
+
+    public Cargo getCargo() {
+        return dadosPessoais.getCargo();
+    }
+
+    public void setCargo(Cargo cargo) {
+        dadosPessoais.setCargo(cargo);
+    }
+
+    public BigDecimal getSalario() {
+        return dadosPessoais.getSalario();
+    }
+
+    public void setSalario(BigDecimal salario) {
+        dadosPessoais.setSalario(salario);
+    }
+
 }
